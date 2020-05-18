ddf460a0516d8434496ad00bb7ffda8d0fa6c8ef
hbjs97 <57822562+hbjs97@users.noreply.github.com>
2020-05-19 02:39:56 +0900

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,8 +1,16 @@
 import time
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome("./driver/chromedriver")
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('--disable-gpu')
+
+driver = webdriver.Chrome("./driver/chromedriver", options=options)
 
 #로그인
 driver.get('https://yes.knu.ac.kr/comm/comm/support/login/login.action')
@@ -14,7 +22,6 @@ driver.find_element_by_xpath('//*[@id="loginBtn"]').click()
 
 #상담페이지
 driver.get('https://yes.knu.ac.kr/stud/smar/advcStu/stuAdvcAll/list.action')
-time.sleep(2)
 html_consult = driver.page_source
 soup_consult = bs(html_consult, 'html.parser')
 
@@ -27,13 +34,16 @@ print(consult)  #총 상담횟수
 
 #성적페이지
 driver.get('https://yes.knu.ac.kr/cour/scor/certRec/certRecEnq/list.action')
-time.sleep(2)
+try:
+    WebDriverWait(driver, 10).until(
+         EC.presence_of_element_located((By.CSS_SELECTOR, "#certRecEnqGrid > div.title")))
+except TimeoutException:
+    print("10sec, time out")
 html = driver.page_source
 soup = bs(html, 'html.parser')
 
 #파싱
 data = soup.find('div', {'id': 'content'})
-
 #학적정보//data1, list1
 data1 = data.find('table', {'class': 'form4'})
 list1 = data1.findAll('td')
@@ -47,7 +57,7 @@ print(stud_info)
 data2 = data.find('div', {'id': 'certRecEnqGrid'})
 list2 = data2.findAll('td')
 score_info = []
-group_num = int(len(list2)/7)
+group_num = int(len(list2)/7)   #강의 갯수
 i = 0
 for j in range(0, group_num):
     score_info.append([list2[i].text,
@@ -59,3 +69,4 @@ for j in range(0, group_num):
     i = i+7
 
 print(score_info)
+driver.quit()
