c377f2e1df50228f238dcdde339b5ccffe42c0ff
hbjs97 <57822562+hbjs97@users.noreply.github.com>
2020-06-24 21:29:52 +0900

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import requests
 from mysql.connector import errorcode
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
@@ -7,23 +8,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def Insert_DB(score_info, list2) :
-    config = {
+config = {
         "user": "root",
         "password": "root",
         "host": "127.0.0.1",
         "database": "hbjs",
         "port": "3306"
     }
+
+def Insert_DB(score_info, list2) :
     try :
         # db 연결 객체 생성
         conn = mysql.connector.connect(**config, charset='utf8')
         # SQL 실행 객체 생성
         cur = conn.cursor()
         i=0
-        for i in range(i, int(len(list2)/7)):
-            sql = 'INSERT INTO test (id, stud_name, major, state, course, year, subject, code, sub_name, score, grade) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
-            cur.execute(sql, (stud_info[0], stud_info[1], stud_info[2], stud_info[3], stud_info[4], score_info[i][0], score_info[i][1], score_info[i][2], score_info[i][3], score_info[i][4], score_info[i][5]))
+        for i in range(0, len(list2)-1):
+            sql = 'INSERT INTO test (id, stud_name, major, state, course, year, subject, code, sub_name, score, grade, grade_num) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+            cur.execute(sql, (stud_info[0], stud_info[1], stud_info[2], stud_info[3], stud_info[4], score_info[i][0], score_info[i][1], score_info[i][2], score_info[i][3], score_info[i][4], score_info[i][5], score_info[i][6]))
             conn.commit()
         # DB 연결 예외 처리
     except mysql.connector.Error as err:
@@ -38,13 +40,6 @@ def Insert_DB(score_info, list2) :
         conn.close()
 
 def Retrieve_DB():  #지금은 출력, 파라미터와 리턴수정해 필요한 데이터 로드.
-    config = {
-        "user": "root",
-        "password": "root",
-        "host": "127.0.0.1",
-        "database": "hbjs",
-        "port": "3306"
-    }
     try:
         conn = mysql.connector.connect(**config)
         print(conn)
@@ -68,33 +63,31 @@ def Retrieve_DB():  #지금은 출력, 파라미터와 리턴수정해 필요한
         conn.rollback()  # 롤백 처리
     finally :
         conn.close()
-
-
-# ChromeDriver(https://chromedriver.chromium.org/downloads)가 저장된 경로.
-path = './chromedriver'
+        
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(path, options=options)
+driver = webdriver.Chrome("./driver/chromedriver", options=options)
 
 #로그인
-driver.get('https://yes.knu.ac.kr/comm/comm/support/login/login.action')
 id = input('ID: ')
 passwd = input('passwd: ')
-driver.find_element_by_name('user.usr_id').send_keys(id)
-driver.find_element_by_name('user.passwd').send_keys(passwd)
-driver.find_element_by_xpath('//*[@id="loginBtn"]').click()
+session = requests.session()
+res = session.post('https://yes.knu.ac.kr/comm/comm/support/login/login.action',
+                   data={'user.usr_id': id, 'user.passwd': passwd})
+res.raise_for_status()
+driver.get('https://yes.knu.ac.kr/')
+
+for c in session.cookies:
+    driver.add_cookie({'name': c.name, 'value': c.value})
+driver.refresh()
 
 #상담페이지
-driver.get('https://yes.knu.ac.kr/stud/smar/advcStu/stuAdvcAll/list.action')
-html_consult = driver.page_source
-soup_consult = bs(html_consult, 'html.parser')
-
-#파싱
-data_consult = soup_consult.find('table', {'class': 'form4'})
-tmp1 = data_consult.find('td', {'style': 'color:red;font-weight: bold;'})
-consult = tmp1.getText()
-print(consult)  #총 상담횟수
+res = session.post('https://yes.knu.ac.kr/stud/smar/advcStu/stuAdvcAll/list.action')
+html = res.text
+soup = bs(html, 'html.parser')
+advc = soup.select_one('.form4 td').text
+print('상담 : ' + advc)
 
 #성적페이지
 driver.get('https://yes.knu.ac.kr/cour/scor/certRec/certRecEnq/list.action')
@@ -102,36 +95,34 @@ try:
     WebDriverWait(driver, 10).until(
          EC.presence_of_element_located((By.CSS_SELECTOR, "#certRecEnqGrid > div.title")))
 except TimeoutException:
-    print("10sec, time out")
+    print("Time out")
 html = driver.page_source
 soup = bs(html, 'html.parser')
 
-#파싱
-data = soup.find('div', {'id': 'content'})
-#학적정보//data1, list1
-data1 = data.find('table', {'class': 'form4'})
-list1 = data1.findAll('td')
+#학적정보//list1
+list1 = soup.select('#studInfo td')
 stud_info = []
-for i in range(0, len(list1)):
-    stud_info.append(list1[i].getText())
+for i in list1:
+    stud_info.append(i.text)
 #stud_info[0~4] : 학번, 이름, 학과, 학적상태, 과정구분
 print(stud_info)
 
-#전체성적//data2, list2
-data2 = data.find('div', {'id': 'certRecEnqGrid'})
-list2 = data2.findAll('td')
+list2 = soup.select('#certRecEnqGrid .data')
 score_info = []
-group_num = int(len(list2)/7)   #강의 갯수
-i = 0
-for j in range(0, group_num):
-    score_info.append([list2[i].text,
-                       list2[i+1].text,
-                       list2[i+2].text,
-                       list2[i+3].text,
-                       list2[i+4].text,
-                       list2[i+5].text])
-    i = i+7
+for i in list2:
+    subject = []
+    subject.append(i.select_one('.yr_trm').text)  # 년도/학기
+    subject.append(i.select_one('.subj_div_cde').text)  # 교과구분
+    subject.append(i.select_one('.subj_cde').text)  # 과목코드
+    subject.append(i.select_one('.subj_nm').text)  # 과목명
+    subject.append(i.select_one('.unit').text)  # 학점
+    subject.append(i.select_one('.rec_rank_cde').text)  # 점수
+    subject.append(i.select_one('.grd').text)  # 평점
+    score_info.append(subject)
+del score_info[0]  # 빈 리스트(구분) 삭제
+
 print(score_info)
 driver.quit()
+
 Insert_DB(score_info, list2)
 Retrieve_DB()
