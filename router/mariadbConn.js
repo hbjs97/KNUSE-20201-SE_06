9e77adc9e2ae2f9e5ddbbc5493d6d31b8ea3cdf5
572915 <572915@naver.com>
2020-06-30 16:16:14 +0000

    make charts

--- router/mariadbConn.js
+++ router/mariadbConn.js
@@ -4,8 +4,9 @@ var pool = mysql.createConnection({
     port: 3307,
     user: 'software',
     password: '1q2w3e4r!',
-    database: 'my_db',
-    connectionLimit: 10
+    database: 'hbjs',
+    connectionLimit: 100,
+    multipleStatements: true
 });
 console.log('dbconn');
 module.exports = pool;
